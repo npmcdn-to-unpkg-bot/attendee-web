6f15fabb5e558c89315a6315bed804df9fc39e2b
Lara Janecka <numbersandlettersandallthat@gmail.com>
2016-07-09 22:33:36 -0400

    Basic event information

--- src/js/pages/Events.js
+++ src/js/pages/Events.js
@@ -1,10 +1,43 @@
 import React from "react";
 
+import Event from "../components/Event";
+import EventStore from "../stores/EventStore";
+
+
 export default class Events extends React.Component {
+	constructor() {
+		super();
+		this.getEvents = this.getEvents.bind(this);
+		this.state = {
+			events: EventStore.getAll()
+		}
+	}
+
+  componentWillMount() {
+    EventStore.on("change", this.getEvents);
+  }
+
+  componentWillUnmount() {
+    EventStore.removeListener("change", this.getEvents);
+  }
+
+	getEvents() {
+		this.setState({
+			events: EvenStore.getAll()
+		})
+	}
+
   render() {
+    const { events } = this.state;
+
+    const EventComponents = events.map((event) => {
+        return <Event key={event.id} {...event}/>;
+    });
+
     return (
       <div>
         <h1>Events</h1>
+        <ul>{EventComponents}</ul>
       </div>
     );
   }
--- src/js/stores/EventStore.js
+++ src/js/stores/EventStore.js
@@ -4,65 +4,51 @@ import dispatcher from "../dispatcher";
 
 class EventStore extends EventEmitter {
   constructor() {
-    super();
-
-    //TEMP! These messages will come from the server
-    this.events = [{
-      "id": 113464613,
-      "name": "Event Creation",
-      "description": "This a gathering of developers working to create dummy data for displaying events",
-      "stream": 7,
-      "isSubscribed": true,
-      "location": "That one place",
-      "time": {
-        "start": 1470300811,
-        "end": 1470300911
+    super()
+    this.events = [
+      {
+        id: 113464613,
+        name: "Event Creation",
+        description: "This a gathering of developers working to create dummy data for displaying events",
+        stream: 7,
+        isSubscribed: true,
+        location: "That one place",
+        time: {
+          start: 1451606400000,
+          end: 1451610000000
+        }
+      },{
+        id: 113464614,
+        name: "Event Creation pt 2",
+        description: "This the second gathering of developers working to create dummy data for displaying events",
+        stream: 7,
+        isSubscribed: false,
+        location: "That other place",
+        time: {
+          start: 1451620800000,
+          end: 1451624400000
+        }
+      },{
+        id: 213464614,
+        name: 12,
+        description: "'Cause who doesn't like cats",
+        stream: "HAPPINESS",
+        isSubscribed: true,
+        location: "Paradise",
+        time: {
+          start: 1451671200000,
+          end: 1451678400000
+        }
       }
-    },
-    {
-      "id": 2,
-      "name": "Test Event 2",
-      "description": "This a gathering of developers working to create dummy data for displaying events",
-      "stream": 7,
-      "isSubscribed": false,
-      "location": "That other place",
-      "time": {
-        "start": 1470400811,
-        "end": 1470450811
-      }
-    },
-    {
-      "id": 4,
-      "name": "Test Event 3",
-      "description": "This a gathering of developers working to create dummy data for displaying events",
-      "stream": 42,
-      "isSubscribed": false,
-      "location": "That other other place",
-      "time": {
-        "start": 1480400811,
-        "end": 1490400811
-      }
-    }]
+    ];
   }
 
-  getEvents()
-  {
-    //TEMP! Will return events them from the server
+  getAll() {
     return this.events;
-
   }
 
-  manageEvents(action) {
-    switch(action.type) {
-      case "REFRESH_EVENTS": {
-        this.getEvents();
-        break;
-      }
-    }
-  }
 }
 
 const eventStore = new EventStore;
-dispatcher.register(eventStore.manageEvents.bind(eventStore));
 
 export default eventStore;
