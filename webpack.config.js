578803e63c1fd51c6eee2f43fa41921ebbd3cc4c
Lara Janecka <numbersandlettersandallthat@gmail.com>
2016-07-10 00:19:43 -0400

    dependencies are hard so sass broke :(

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 
 module.exports = {
   context: path.join(__dirname, "src"),
