942b133e5a9d7881ca8b318fe8d8c91c1ef7fd3c
Lara Janecka <numbersandlettersandallthat@gmail.com>
2016-07-21 17:59:55 -0400

    Adding dynamic routes

--- src/js/client.js
+++ src/js/client.js
@@ -1,26 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router, Route, IndexRoute, hashHistory } from "react-router";
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
 
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import NewsFeed from "./pages/NewsFeed";
 import People from "./pages/People";
 import Events from "./pages/Events";
+import EventPage from "./pages/Event";
 import Profile from "./pages/Profile";
 import Calendar from "./pages/Calendar";
 import Info from "./pages/Info";
+
+import { createHistory } from 'history'
+const history = createBrowserHistory()
 
 
 const app = document.getElementById('app');
 
 ReactDOM.render(
-  <Router history={hashHistory}>
+  <Router history={history}>
     <Route path="/" component={Layout}>
       <IndexRoute component={Home}></IndexRoute>
       <Route path="newsfeed" component={NewsFeed}></Route>
       <Route path="people" component={People}></Route>
       <Route path="events" component={Events}></Route>
+      <Route path="events/:eventId" component={EventPage}></Route>
       <Route path="profile" component={Profile}></Route>
       <Route path="calendar" component={Calendar}></Route>
       <Route path="info" component={Info}></Route>
--- src/js/stores/EventStore.js
+++ src/js/stores/EventStore.js
@@ -3,6 +3,9 @@ import { EventEmitter } from "events";
 import dispatcher from "../dispatcher";
 
 var $ = require("jquery");
+var randomColor = require('randomcolor');
+var streamMap = {};
+
 
 class EventStore extends EventEmitter {
   constructor() {
@@ -26,17 +29,52 @@ class EventStore extends EventEmitter {
     return this.events;
   }
 
+  get(id){
+    $.ajax({
+      url: " https://sehackday.calligre.com/api/event",
+      dataType: "json",
+      cache: false,
+      success: function(response){
+        dispatcher.dispatch({type: "EVENT_GET", event: response});
+      },
+      failure: function(error){
+        dispatcher.dispatch({type: "ERROR", error: error});
+      }
+    });
+    return this.events;
+  }
+
   handleActions(action) {
     switch(action.type) {
       case "EVENTS_GET": {
-        this.events = action.events;
+        action.events.forEach((event) => {
+          if(typeof streamMap[event.stream] == "undefined"){
+            streamMap[event.stream] = randomColor();
+          }
+        });
+
+        this.events = action.events.map((event) => {
+          event["streamColor"] = streamMap[event.stream];
+          return event;
+        })
+
         this.emit("received");
         break;
       }
-      case "EVENTS_ERROR":
+      case "EVENT_GET": {
+        this.events.forEach((event) => {
+          if(event.id == data.id) {
+            $.extend(event, data);
+          }
+        });
+        this.emit("received");
+        break;
+      }
+      case "ERROR": {
         this.error = action.error;
         this.emit("error");
         break;
+      }
     }
   }
 
