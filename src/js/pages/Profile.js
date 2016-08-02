ea182d706a17d608b0b63808521a2b182f07dcf0
Clarisse Schneider <cmschnei@uwaterloo.ca>
2016-08-02 19:59:17 -0400

    make own profile look editable

--- src/js/pages/Profile.js
+++ src/js/pages/Profile.js
@@ -3,6 +3,7 @@ import React from "react";
 import PeopleStore from "../stores/PeopleStore";
 import Dropzone from 'react-dropzone';
 import Clear from 'react-icons/lib/md/clear';
+import Edit from 'react-icons/lib/md/mode-edit';
 
 export default class Profile extends React.Component {
   constructor(props) {
@@ -97,26 +98,33 @@ export default class Profile extends React.Component {
 
     // TODO: determine if this is my profile or not
     const myProfile = id == 1;
-    const displayUpdate = myProfile ? "visible" : "hidden";
+
     const displayCancel = myProfile && this.state.uploadInProgress ? "visible" : "hidden";
+    const myProfileClass = myProfile ? "myProfile" : "";
     
     const buttonIcon = React.createElement(Clear, null);
+    const editIcon = React.createElement(Edit, null);
 
     return (
-      <div className="profile">
+      <div className={"profile " + myProfileClass}>
         <Dropzone className='dropzone' onDrop={this.onDrop} multiple={false} disableClick={!myProfile}>
           <img src={this.state.preview}/>
           <button className={"cancel " + displayCancel}  onClick={this.cancelDrop}>{buttonIcon}</button>
-          <p className={"label " + displayUpdate}>Upload new photo</p>
+          <p className="label">Upload new photo</p>
         </Dropzone>
         <h2>{first_name} {last_name}</h2>
-        <h3 contentEditable={myProfile} className="organization">{organization}</h3>
         <h4>Points: {points}</h4>
-        <div className="socialMediaContainer">
+        <div className="editableContainer profileItem">
+          <h3 contentEditable={myProfile} className="organization editable">{organization}</h3>
+          <div className="editIcon">{editIcon}</div>
         </div>
-        <p contentEditable={myProfile} className="description">{description}</p>
+        <div className="socialMediaContainer profileItem"></div>
+        <div className="editableContainer profileItem">
+          <p contentEditable={myProfile} className="description editable">{description}</p>
+          <div className="editIcon">{editIcon}</div>
+        </div>
 
-        <button lassName={"submitChanges " + displayUpdate} onClick={this.submitChanges}>Save changes</button>
+        <button className="submitChanges" onClick={this.submitChanges}>Save changes</button>
       </div>
     );
   }
